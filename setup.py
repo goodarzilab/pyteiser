188d2e0e509d6e189dd4bc9787402f6bdb1e27fe
khorms <khorms21@gmail.com>
2019-07-30 16:00:37 -0700

    addede setup py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyteiser",
+    version="0.0.1",
+    author="Matvei Khoroshkin, Hani Goodarzi",
+    author_email="khorms21@gmail.com",
+    description="Python implementation of TEISER",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/goodarzilab/pyteiser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
