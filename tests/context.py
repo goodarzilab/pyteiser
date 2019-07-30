540577d14e923dda11c888a59ee2a4091e6a4dae
khorms <khorms21@gmail.com>
2019-07-30 16:17:02 -0700

    added folder structure

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+# see how it's implemented here: https://www.kennethreitz.org/essays/repository-structure-and-python
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import pyteiser
