1eb70bf647ea96f19ff583d2b804cb18e1d65549
khorms <khorms21@gmail.com>
2019-08-06 23:50:19 -0700

    adddd structures.h

---
+++ pyteiser/seeds_generator.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import structures
+
+def get_next_motif(motif, last_letter = 'N'):
+    for i in range(motif.linear_length):
+        if motif[i].nucl < last_letter:
+            numpy uint8 next one
+
+
+aa = structures.s_motif()
+
+
--- pyteiser/structures.py
+++ pyteiser/structures.py
@@ -1,6 +1,28 @@
+import numpy as np
+
+
+# !
+# !
+# !
+# !
+# !
+# I only need a class for motif. It will contain one numpy array of uint8 for nucleotides and another one for structure
+# I don't need separate classes for sequence, phrace and nucbit
+# !
+# !
+# !
+# !
+# !
+
+
+class nucbit:
+    def __init__(self, character):
+        self.nucl =
+
+
 class s_phrase:
 
-    def __init__(self, dict):
+    def __init__(self):
         self.base = 0 # NUCBIT - byte
         # _U | _C | _G | _A; if structure = _pair, base shows the left nucleotide in the paired bases
         self.structure = 0 # byte: _pair ^ _leftBulge ^ _rightBulge
@@ -8,7 +30,7 @@ class s_phrase:
 
 class s_sequence:
 
-    def __init__(self, dict):
+    def __init__(self):
         self.name = ""
         self.bases = "" #list of NUCBITs
         self.length = 0
@@ -16,13 +38,55 @@ class s_sequence:
 
 class s_motif:
 
-    def __init__(self, dict):
+    def __init__(self, length):
         self.phrases = [] # list of s_phrase objects
         self.num_phrases = 0
         self.linear_length = 0
 
+    def create_structure(self, stem_length, loop_length):
+        # creates a stem-loop structure with the specified stem length and loop length
+        # all bases will be N
+        self.num_phrases = stem_length + loop_length
+        self.linear_length = (stem_length * 2) + loop_length
+        self.phrases = np.ones((2,5), dtype='Int32')
 
 
+# Pseudocode of lcl_create_motifs:
+# the list we are putting all the motifs to is named motifs
+# num_phrases = stem_length + loop_length
+# lcl_initialize_sequence: initialize an array of NUCBITs of the length num_phrases and fill it up with 1s (U)
+# Loop begins
+# lcl_count_informative_bases: counts the number of bases that are not N and put it into num_inf_bases
+# exit point: if( num_inf_bases < min_inf_bases or num_inf_bases < max_inf_bases ): skip this motif
+# create_structure
+# lcl_copy_sequence
+# I = calcluate_information(motif)
+# exit point: if( I < minI || I > maxI ): skip this motif
+# if( num_motifs >= num_motifs_per_file ): flush buffer to file
+# lcl_next_sequence( sequence, num_phrases )
+# Loop ends
+#
+# Pseudocode of create_and_write_motifs:
+# Loop 1 begins:
+# for stem_length in range( min_stem_length, max_stem_length)
+# Loop 2 begins:
+# for loop_length in range( min_loop_length, max_loop_length)
+# Loop 1 ends
+# Loop 2 ends
+#
+#
+#
+#
+#
+#
+#
 
+#define _U				0x01 -> 01
+#define _C				0x02 -> 010
+#define _G				0x04 -> 0100
+#define _A				0x08 -> 01000
+#define _N				0x0F -> 01111
+#define NUM_LETTERS		4
+#define LAST_LETTER		_A
 
 
