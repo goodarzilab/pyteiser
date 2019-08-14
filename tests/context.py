5e07a035b35592f8d99afd9af393f2b3dc73f8cf
khorms <khorms21@gmail.com>
2019-08-13 21:19:47 -0700

    removed context

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-# see how it's implemented here: https://www.kennethreitz.org/essays/repository-structure-and-python
-
-import os
-import sys
-import numpy as np
-sys.path.insert(0, os.path.abspath('..'))
-
-import pyteiser
-
