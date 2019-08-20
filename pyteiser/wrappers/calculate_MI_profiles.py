3780c359cf5a1b7bae2f2ce0bd9b674a5f3deda8
khorms <khorms21@gmail.com>
2019-08-19 23:10:40 -0700

    mut info testing

--- pyteiser/wrappers/calculate_MI_profiles.py
+++ pyteiser/wrappers/calculate_MI_profiles.py
@@ -13,6 +13,7 @@ if subpackage_folder_path not in sys.path:
     sys.path.append(subpackage_folder_path)
 
 import IO
+import MI
 import matchmaker
 import type_conversions
 
@@ -22,12 +23,13 @@ def handler():
 
     parser.add_argument("--profiles_bin_file", type=str)
     parser.add_argument("--exp_mask_file", help="", type=str)
+    parser.add_argument("--nbins", help="", type=int)
 
     parser.set_defaults(
-        #profiles_bin_file="/Users/student/Documents/hani/programs/pyteiser/data/test_profiles/test_motifs_101.bin",
-        profiles_bin_file="/Users/student/Documents/hani/programs/pyteiser/data/test_profiles/profiles_4-7_4-9_4-6_14-20_30k_1.bin",
-
-        exp_mask_file='/Users/student/Documents/hani/programs/pyteiser/data/mask_files/TARBP2_decay_t_score_mask.bin'
+        profiles_bin_file="/Users/student/Documents/hani/programs/pyteiser/data/test_profiles/test_motifs_101.bin",
+        #profiles_bin_file="/Users/student/Documents/hani/programs/pyteiser/data/test_profiles/profiles_4-7_4-9_4-6_14-20_30k_1.bin",
+        exp_mask_file='/Users/student/Documents/hani/programs/pyteiser/data/mask_files/TARBP2_decay_t_score_mask.bin',
+        nbins = 3,
     )
 
     args = parser.parse_args()
@@ -60,23 +62,30 @@ def unpack_profiles_and_mask(args, do_print=False):
     return decompressed_profiles_array, index_array, values_array
 
 
-def quantize_values(index_array, values_array):
-    quant_values_array = values_array[index_array]
-    print(quant_values_array)
+def quantize_values(index_array, values_array, nbins):
+    active_values_array = values_array[index_array]
+    quant_values_array = MI.discretize(active_values_array, bins=nbins, disc = "equalfreq")
     return quant_values_array
 
 
 
 def calculate_MI_wrapper(decompressed_profiles_array, index_array, quant_values_array):
     for profile in decompressed_profiles_array:
-        pass
+        active_profile = profile[index_array]
+        current_MI = MI.mut_info(active_profile, quant_values_array)
+        print(current_MI)
+
+
+
 
 
 def main():
     args = handler()
     decompressed_profiles_array, index_array, values_array = unpack_profiles_and_mask(args, do_print=True)
-    quant_values_array = quantize_values(index_array, values_array)
+    quant_values_array = quantize_values(index_array, values_array, args.nbins)
     calculate_MI_wrapper(decompressed_profiles_array, index_array, quant_values_array)
+
+    # proceed with line 179 in mi_find_seed.c
 
 
 
